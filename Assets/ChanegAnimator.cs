8db9d5e2ec750b3a94a4fcb846ba9389d309979b
phanitporn <phanitporn.pinta@gmail.com>
2022-02-27 19:38:43 +0700

    first commit

---
+++ Assets/ChanegAnimator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChanegAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void getAnimatorVar1(){
+       this.GetComponent<Animator>().Play("Breakdance Uprock Var 1");
+   }
+   public void getAnimatorVar3(){
+       this.GetComponent<Animator>().Play("Breakdance Freeze Var 3");
+   }
+}
